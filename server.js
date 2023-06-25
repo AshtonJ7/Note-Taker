273e18873da33cd919aa2d3d71901bddbc7854e5
AshtonJ7 <AshtonJ7@users.noreply.github.com>
2023-06-25 21:06:46 +0100

    notes.js added and server.js updated

--- server.js
+++ server.js
@@ -2,12 +2,12 @@
 const express = require('express');
 const path = require('path');
 const api = require('./routes/index.js')
-// app use express
-const app = express();
+
 
 // creating environment variable port
 const PORT = process.env.PORT || 3001;
-
+// app use express
+const app = express();
 
 // parsing Json 
 app.use('/api', api)
@@ -15,16 +15,16 @@ app.use('/api', api)
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
 // routes to route files
 app.get('/', (req, res) =>
-res.sendFile(path.join(_diename, '/public/index.html')))
+res.sendFile(path.join(__dirname, '/public/index.html')))
 
 app.get('/notes', (req, res) =>
-res.sendFile(path.join(_diename, '/public/notes.html')))
+res.sendFile(path.join(__dirname, '/public/notes.html')))
 
 // app listener - starts the server
-app.listen(PORT, () => {
-  console.log(`Server available at localhost${PORT}`);
-});
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
